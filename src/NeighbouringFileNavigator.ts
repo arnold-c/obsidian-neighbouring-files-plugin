3021efe78534c4bab4990421576b347d8332b6b5
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2024-05-28 17:51:05 -0400

    Update NeighbouringFileNavigator.ts to remove md requirement
    Want to be able to switch through pdf files

--- src/NeighbouringFileNavigator.ts
+++ src/NeighbouringFileNavigator.ts
@@ -4,7 +4,7 @@ export default class NeighbouringFileNavigator {
 
 	public static getNeighbouringFiles(file: TFile) {
 		const files = file?.parent?.children;
-		const filteredFiles = files?.filter(f => f instanceof TFile && f.extension === 'md') as TFile[];
+		const filteredFiles = files?.filter(f => f instanceof TFile) as TFile[];
 		const sortedFiles = filteredFiles?.sort((a, b) =>
 			a.basename.localeCompare(b.basename, undefined, {
 				numeric: true,
